5dca5ee92561820e736f45c3c0b1dad5f6d34c45
mikhail-vnukov <mikhail.vnukov@gmail.com>
2015-02-25 00:18:55 +0300

    Finished codings 1

--- src/formdata.php
+++ src/formdata.php
@@ -4,73 +4,71 @@
 
 	error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
-	if(isset($_POST['submitted'])) {
+	if (!isset($_POST['name']) && !isset($_POST['phone']) && !isset($_POST['email'])) {
+		header('HTTP/1.1 400 Bad Request', true, 400);
+		exit;
+	}
 
-		if (!isset($_POST['name']) && !isset($_POST['phone']) && !isset($_POST['email'])) {
-			header('HTTP/1.1 400 Bad Request', true, 400);
-			exit;
+	// require a name from user
+	$name = trim($_POST['name']);
+	$phone = trim($_POST['phone']);
+	$email = trim($_POST['email']);
+
+
+	if (($phone === '') && ($email === '')) {
+		header('HTTP/1.1 400 Bad Request', true, 400);
+		exit;
+	}
+
+	if (isset($_POST['model'])) {
+		if(!(trim($_POST['model']) === '')) {
+			$model = trim($_POST['model']);
 		}
+	}
 
-		// require a name from user
-		$name = trim($_POST['name']);
-		$phone = trim($_POST['phone']);
-		$email = trim($_POST['email']);
+	// upon no failure errors let's email now!
 
+	// $emailTo = 'mikhail.vnukov@gmail.com';
+	$emailTo = 'horudspb@gmail.com';
+	$subject = 'Новая заявка: '.$name;
+	$body = "Имя: $name \r\nТелефон: $phone \r\nEmail: $email";
 
-		if (($phone === '') && ($email === '')) {
-			header('HTTP/1.1 400 Bad Request', true, 400);
-			exit;
+	if (isset($model)) {
+		$body .= "\r\nМодель: ";
+		switch ($model) {
+			case 'OP-2':
+				$body .= 'ОП-2 (з) автомобильный';
+				break;
+			case 'OU-2':
+				$body .= 'ОУ-2 автомобильный';
+				break;
+			case 'OU-3':
+				$body .= 'ОУ-3 офисы, музеи, лаборатории';
+				break;
+			case 'OP-4':
+				$body .= 'ОП-4 (з) офисы, магазины';
+				break;
+			case 'OP-5':
+				$body .= 'ОП-5 (з) офисы, магазины';
+				break;
+			case 'OU-5':
+				$body .= 'ОУ-5 помещения с электрооборудованием';
+				break;
+
 		}
+	}
 
-		if (isset($_POST['model'])) {
-			if(!(trim($_POST['model']) === '')) {
-				$model = trim($_POST['model']);
-			}
-		}
+	$headers = 'From: <postmaster@spbpoj.ru>' . "\r\n" . 'Reply-To: ' . $email .
+			"\r\nMIME-Version: 1.0" . "\r\n" .
+					"Content-type: text/plain; charset=UTF-8" . "\r\n";
 
-		// upon no failure errors let's email now!
-
-		$emailTo = 'mikhail.vnukov@gmail.com';
-		// $emailTo = 'horudspb@gmail.com';
-		$subject = 'Новая заявка: '.$email;
-		$body = "Имя: $name \r\nТелефон: $phone \r\nEmail: $email";
-
-		if (isset($model)) {
-			$body .= "\r\nМодель: ";
-			switch ($model) {
-				case 'OP-2':
-					$body .= 'ОП-2 (з) автомобильный';
-					break;
-				case 'OU-2':
-					$body .= 'ОУ-2 автомобильный';
-					break;
-				case 'OU-3':
-					$body .= 'ОУ-3 офисы, музеи, лаборатории';
-					break;
-				case 'OP-4':
-					$body .= 'ОП-4 (з) офисы, магазины';
-					break;
-				case 'OP-5':
-					$body .= 'ОП-5 (з) офисы, магазины';
-					break;
-				case 'OU-5':
-					$body .= 'ОУ-5 помещения с электрооборудованием';
-					break;
-
-			}
-		}
-
-		$headers = 'From: <postmaster@spbpoj.ru>' . "\r\n" . 'Reply-To: ' . $email .
-				"\r\nMIME-Version: 1.0" . "\r\n" .
-						"Content-type: text/plain; charset=UTF-8" . "\r\n";
-
-		mail($emailTo, $subject, $body, $headers);
-
-
-
+	if(mail($emailTo, $subject, $body, $headers)) {
 		echo $emailTo;
 		echo $subject;
 		echo $body;
 		echo $headers;
+	} else {
+		echo "Error sending email";
+		header('HTTP/1.1 400 Bad Request', true, 400);
 	}
 ?>
